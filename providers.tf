b1a2e9269c5571354fe9889804e796874d43e036
AlexisSalazar <alexis_13rafael@hotmail.com>
2022-06-02 16:47:25 -0500

    changes to main.tf and providers.tf

---
+++ providers.tf
@@ -0,0 +1,26 @@
+terraform {
+  required_providers {
+    google = {
+      source = "hashicorp/google"
+      version = "4.22.0"
+    }
+  }
+  required_providers = "~> 1.0"
+
+  backend "remote" {
+    organization = "terraform-personal-study"
+
+    workspaces {
+      name = "demo-github-actions" 
+    }
+  }
+}
+
+
+provider "google" {
+  # Configuration options
+  project = "terraform-gcp-351615"
+  region = "us-central1"
+  zone = "us-central1-a"
+  credentials = "key.json"
+}
